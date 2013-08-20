48dc9e7aebc59f9dda0c75e5e3099dab4c9d63ba
FliegenKLATSCH <chris@koras.de>
2013-08-20 22:32:57 +0200

    added speed effect

--- Assets/Planet/startPan.cs
+++ Assets/Planet/startPan.cs
@@ -6,18 +6,18 @@ public class startPan : MonoBehaviour {
 	public Vector3 targetPos = new Vector3(0, 3.615212f, -0.5231594f);
 	public Vector3 targetRot = new Vector3(81, 0, 0);
 	public float targetClip = 1;
+	public int targetFoV = 81;
 	
-	private bool effect;
+	private int effect;
+	private int phase = 0;
 	
-	// Use this for initialization
 	void Start () {
-		effect = Random.Range(0,2) == 0;
+		effect = Random.Range(0,3);
 		
-		if (effect)
+		if (effect == 1)
 			camera.nearClipPlane = 6;
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		if (Input.anyKeyDown) {
 			transform.position = targetPos;
@@ -26,14 +26,23 @@ public class startPan : MonoBehaviour {
 			Destroy(this);
 		}
 			
-		if (effect && camera.nearClipPlane > targetClip) {
+		if (effect == 1 && camera.nearClipPlane > targetClip) { // Planet "aufbauen"
 			transform.position = Vector3.Slerp(transform.position, targetPos, Time.deltaTime*4);
 			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(targetRot), Time.deltaTime * 8);
-			//transform.position = targetPos;
-			//transform.rotation = Quaternion.Euler(targetRot);
 			
 			camera.nearClipPlane -= 0.05f;
-		} else {
+		} else if (effect == 2) { // Lichtgeschwindigkeit
+			transform.position = Vector3.Slerp(transform.position, targetPos, Time.deltaTime*4);
+			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(targetRot), Time.deltaTime * 8);
+			
+			if (phase == 0)
+				camera.fieldOfView = Mathf.Lerp(camera.fieldOfView, 180, Time.deltaTime * 12);
+			else
+				camera.fieldOfView = Mathf.Lerp(camera.fieldOfView, targetFoV, Time.deltaTime);
+			
+			if (camera.fieldOfView >= 179)
+				phase = 1;
+		} else { // Schwenk nach unten
 			camera.nearClipPlane = targetClip;
 			
 			transform.position = Vector3.Slerp(transform.position, targetPos, Time.deltaTime);
